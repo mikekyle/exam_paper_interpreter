a3d487a0c55e4706e2e2a10c75edbe24c3cbc890
mikekyle <mikekyle@hotmail.co.uk>
2021-11-13 12:31:30 +0000

    building downloader

---
+++ paper_downloader.py
@@ -0,0 +1,12 @@
+import requests
+
+SA2urllist = [
+    'https://www.actuaries.org.uk/documents/subject-sa2-life-insurance-specialist-advanced-exam-paper-september-2021',
+    'https://www.actuaries.org.uk/documents/subject-sa2-life-insurance-specialist-advanced-exam-paper-and-examiners-report-april-2021',
+    'https://www.actuaries.org.uk/documents/subject-sa2-life-insurance-advanced-exam-paper-and-examiners-report-september-2020',
+    'https://www.actuaries.org.uk/documents/subject-sa2-life-insurance-advanced-exam-paper-and-examiners-report-april-2020',
+    'https://www.actuaries.org.uk/documents/subject-sa2-life-insurance-advanced-exam-paper-september-2019',
+    'https://www.actuaries.org.uk/documents/subject-sa2-life-insurance-advanced-exam-paper-and-examiners-report-april-2019',
+    'https://www.actuaries.org.uk/documents/subject-sa2-past-exam-papers-and-examiners-reports-2005-2018?aa',
+]
+
